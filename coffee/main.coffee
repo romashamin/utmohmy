57d331a6d4567c876c204c4e4499fc1164acaa4e
Roman Shamin <al4emist.artway@gmail.com>
2014-03-25 18:37:44 +0400

    Add realtime editing, highlighting, emptiness validation

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,19 +1,94 @@
-BUTTON = document.getElementById('btn-submit')
+FORM =
+  input_orig_url    : document.querySelector('#input-orig-url')
+  input_source      : document.querySelector('#input-source')
+  input_channel     : document.querySelector('#input-channel')
+  input_targeting   : document.querySelector('#input-targeting')
+  input_category    : document.querySelector('#input-category')
+  input_subcategory : document.querySelector('#input-subcategory')
 
-INPUT_ORIG_URL    = document.getElementById('input-orig-url')
-INPUT_SOURCE      = document.getElementById('input-source')
-INPUT_CHANNEL     = document.getElementById('input-channel')
-INPUT_TARGETING   = document.getElementById('input-targeting')
-INPUT_CATEGORY    = document.getElementById('input-category')
-INPUT_SUBCATEGORY = document.getElementById('input-subcategory')
+SPANS =
+  input_orig_url    : 'span-orig-url'
+  input_source      : 'span-source'
+  input_channel     : 'span-channel'
+  input_targeting   : 'span-targeting'
+  input_category    : 'span-category'
+  input_subcategory : 'span-subcategory'
 
-RESULT_URL = document.getElementById('result-url')
+RESULT_URL = document.querySelector('#result-url')
+
 
 
 window.onload = ->
 
-  BUTTON.addEventListener('click', build_url)
+  # Set Defaults
+  FORM.input_orig_url.value    = 'ohmystats.com'
+  FORM.input_source.value      = 'ohmyblog'
+  FORM.input_channel.value     = 'cpc'
+  FORM.input_targeting.value   = 'tar ge ting'
+  FORM.input_category.value    = 'cate gory'
+  FORM.input_subcategory.value = 'sub cate gory'
+
+  build_url()
+
+  # Setup event listeners
+  for input_name, input of FORM
+
+    input.addEventListener('input', () ->
+      if @value in [null, '']
+        @classList.add('invalid')
+      else
+        @classList.remove('invalid')
+
+      build_url()
+    )
+
+    input.addEventListener('focus', () ->
+      span_id = "##{@id.replace(/input-/, 'span-')}"
+      document.querySelector(span_id).classList.add('highlighted')
+      @focused = true
+    , false)
+
+    input.addEventListener('blur', () ->
+      span_id = "##{@id.replace(/input-/, 'span-')}"
+      document.querySelector(span_id).classList.remove('highlighted')
+      @focused = false
+    , false)
+
 
 
 build_url = ->
-  RESULT_URL.innerHTML = "#{INPUT_ORIG_URL.value}?utm_source=#{INPUT_SOURCE.value}&utm_medium=#{INPUT_CHANNEL.value}&utm_campaign=#{INPUT_TARGETING.value}_#{INPUT_CATEGORY.value}_#{INPUT_SUBCATEGORY.value}"
+
+  classes =
+    input_orig_url    : ''
+    input_source      : ''
+    input_channel     : ''
+    input_targeting   : ''
+    input_category    : ''
+    input_subcategory : ''
+
+  for input_name, input of FORM
+    if input.focused is true
+      classes[input_name] = 'highlighted'
+    else
+      classes[input_name] = ''
+
+  RESULT_URL.innerHTML = "<span id=#{SPANS.input_orig_url} class=#{classes.input_orig_url}>#{spaces_to_hyphens(FORM.input_orig_url.value)}</span>?utm_source=<span id=#{SPANS.input_source} class=#{classes.input_source}>#{spaces_to_hyphens(FORM.input_source.value)}</span>&utm_medium=<span id=#{SPANS.input_channel} class=#{classes.input_channel}>#{spaces_to_hyphens(FORM.input_channel.value)}</span>&utm_campaign=<span id=#{SPANS.input_targeting} class=#{classes.input_targeting}>#{spaces_to_hyphens(FORM.input_targeting.value)}</span>_<span id=#{SPANS.input_category} class=#{classes.input_category}>#{spaces_to_hyphens(FORM.input_category.value)}</span>_<span id=#{SPANS.input_subcategory} class=#{classes.input_subcategory}>#{spaces_to_hyphens(FORM.input_subcategory.value)}</span>"
+
+
+
+spaces_to_hyphens = (str) -> str.replace /\s+/g, '-'
+
+
+
+###
+validate_form = ->
+  if validity_of FORM.input_orig_url is false
+    FORM.input_orig_url.classList.add('invalid')
+  else
+    FORM.input_orig_url.classList.remove('invalid')
+
+
+
+validity_of = (input) ->
+  if input.value in [null, ''] then return false else return true
+###
--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,100 @@
 (function() {
-  var BUTTON, INPUT_CATEGORY, INPUT_CHANNEL, INPUT_ORIG_URL, INPUT_SOURCE, INPUT_SUBCATEGORY, INPUT_TARGETING, RESULT_URL, build_url;
+  var FORM, RESULT_URL, SPANS, build_url, spaces_to_hyphens;
 
-  BUTTON = document.getElementById('btn-submit');
+  FORM = {
+    input_orig_url: document.querySelector('#input-orig-url'),
+    input_source: document.querySelector('#input-source'),
+    input_channel: document.querySelector('#input-channel'),
+    input_targeting: document.querySelector('#input-targeting'),
+    input_category: document.querySelector('#input-category'),
+    input_subcategory: document.querySelector('#input-subcategory')
+  };
 
-  INPUT_ORIG_URL = document.getElementById('input-orig-url');
+  SPANS = {
+    input_orig_url: 'span-orig-url',
+    input_source: 'span-source',
+    input_channel: 'span-channel',
+    input_targeting: 'span-targeting',
+    input_category: 'span-category',
+    input_subcategory: 'span-subcategory'
+  };
 
-  INPUT_SOURCE = document.getElementById('input-source');
-
-  INPUT_CHANNEL = document.getElementById('input-channel');
-
-  INPUT_TARGETING = document.getElementById('input-targeting');
-
-  INPUT_CATEGORY = document.getElementById('input-category');
-
-  INPUT_SUBCATEGORY = document.getElementById('input-subcategory');
-
-  RESULT_URL = document.getElementById('result-url');
+  RESULT_URL = document.querySelector('#result-url');
 
   window.onload = function() {
-    return BUTTON.addEventListener('click', build_url);
+    var input, input_name, _results;
+    FORM.input_orig_url.value = 'ohmystats.com';
+    FORM.input_source.value = 'ohmyblog';
+    FORM.input_channel.value = 'cpc';
+    FORM.input_targeting.value = 'tar ge ting';
+    FORM.input_category.value = 'cate gory';
+    FORM.input_subcategory.value = 'sub cate gory';
+    build_url();
+    _results = [];
+    for (input_name in FORM) {
+      input = FORM[input_name];
+      input.addEventListener('input', function() {
+        var _ref;
+        if ((_ref = this.value) === null || _ref === '') {
+          this.classList.add('invalid');
+        } else {
+          this.classList.remove('invalid');
+        }
+        return build_url();
+      });
+      input.addEventListener('focus', function() {
+        var span_id;
+        span_id = "#" + (this.id.replace(/input-/, 'span-'));
+        document.querySelector(span_id).classList.add('highlighted');
+        return this.focused = true;
+      }, false);
+      _results.push(input.addEventListener('blur', function() {
+        var span_id;
+        span_id = "#" + (this.id.replace(/input-/, 'span-'));
+        document.querySelector(span_id).classList.remove('highlighted');
+        return this.focused = false;
+      }, false));
+    }
+    return _results;
   };
 
   build_url = function() {
-    return RESULT_URL.innerHTML = "" + INPUT_ORIG_URL.value + "?utm_source=" + INPUT_SOURCE.value + "&utm_medium=" + INPUT_CHANNEL.value + "&utm_campaign=" + INPUT_TARGETING.value + "_" + INPUT_CATEGORY.value + "_" + INPUT_SUBCATEGORY.value;
+    var classes, input, input_name;
+    classes = {
+      input_orig_url: '',
+      input_source: '',
+      input_channel: '',
+      input_targeting: '',
+      input_category: '',
+      input_subcategory: ''
+    };
+    for (input_name in FORM) {
+      input = FORM[input_name];
+      if (input.focused === true) {
+        classes[input_name] = 'highlighted';
+      } else {
+        classes[input_name] = '';
+      }
+    }
+    return RESULT_URL.innerHTML = "<span id=" + SPANS.input_orig_url + " class=" + classes.input_orig_url + ">" + (spaces_to_hyphens(FORM.input_orig_url.value)) + "</span>?utm_source=<span id=" + SPANS.input_source + " class=" + classes.input_source + ">" + (spaces_to_hyphens(FORM.input_source.value)) + "</span>&utm_medium=<span id=" + SPANS.input_channel + " class=" + classes.input_channel + ">" + (spaces_to_hyphens(FORM.input_channel.value)) + "</span>&utm_campaign=<span id=" + SPANS.input_targeting + " class=" + classes.input_targeting + ">" + (spaces_to_hyphens(FORM.input_targeting.value)) + "</span>_<span id=" + SPANS.input_category + " class=" + classes.input_category + ">" + (spaces_to_hyphens(FORM.input_category.value)) + "</span>_<span id=" + SPANS.input_subcategory + " class=" + classes.input_subcategory + ">" + (spaces_to_hyphens(FORM.input_subcategory.value)) + "</span>";
   };
+
+  spaces_to_hyphens = function(str) {
+    return str.replace(/\s+/g, '-');
+  };
+
+
+  /*
+  validate_form = ->
+    if validity_of FORM.input_orig_url is false
+      FORM.input_orig_url.classList.add('invalid')
+    else
+      FORM.input_orig_url.classList.remove('invalid')
+  
+  
+  
+  validity_of = (input) ->
+    if input.value in [null, ''] then return false else return true
+   */
 
 }).call(this);
